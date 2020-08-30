f5fce8862a681db126b0bafbace9a5863112188d
justnat3 <nd2010@reedusa.com>
2020-08-29 22:23:20 -0500

    add IO

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github/justnat3/goDupe
 go 1.15
 
 require (
+	gioui.org v0.0.0-20200829162755-829ee4559c5a // indirect
 	github.com/devedge/imagehash v0.0.0-20180324030135-7061aa3b4066
 	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/zserge/lorca v0.1.9
--- main.go
+++ main.go
@@ -5,38 +5,88 @@ import (
 	//"encoding/hex"
 	"fmt"
 	//"log"
-	"os"
-	"path/filepath"
 	//"github.com/devedge/imagehash"
-	//"io"
+
+	"io/ioutil"
+	"time"
 )
 
-var dir string
-var file string
+var (
+	//dir : INITIAL DIRECTORY
+	dir string
+	//file : files return?
+	file string
+)
 
 //	TODO:1. Be able to scan in a directory and readin each -> image and output -> array hashes
 //	     2. take though hashes and either store them in memory or store them in a local db/dump_file
 //	     3. File hash for every iterate of the loop
 
 func main() {
-	FilePathWalkDir("C:\\git\\test_dir")
+
+	window := app.NewWindow()
+	for e := range window.Events() {
+		switch e := e.(type) {
+		case system.DestroyEvent:
+			// The window was closed.
+			return e.Err
+		case system.FrameEvent:
+			// A request to draw the window state.
+			ops := new(op.Ops)
+			// Draw the state into ops.
+			draw(ops)
+			// Update the display.
+			e.Frame(ops)
+		}
+	}
+
+	files, err := IOReadDir("C:\\git\\test_dir")
+
+	if err != nil {
+		println("Did not Preform IOREAD_DIR Function")
+	}
+
+	time.Sleep(5 * time.Second)
+	IOReadFile(files)
 }
 
-func FilePathWalkDir(root string) ([]string, error) {
+//IOReadFile : Take in files from IOREADDir function and read the bytes to check contentType
+func IOReadFile(files []string) {
+	var read_file string
+	n := 512
+
+	buf := make([]byte, n)
+
+	for i := 0; i < len(files); i++ {
+		fmt.Println("FOR=> " + files[i])
+
+		read_file = files[i]
+		read, err := ioutil.ReadFile(read_file)
+
+		println(read)
+		if err != nil {
+			println(err)
+		}
+	}
+}
+
+//IOReadDir : Read in Directory and spit out file names + PATH
+func IOReadDir(root string) ([]string, error) {
+	fmt.Println("\n")
+
 	var files []string
+	fileInfo, err := ioutil.ReadDir(root)
 	c := 0
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+
+	if err != nil {
+		return files, err
+	}
+
+	println(root + "\\")
+	for _, file := range fileInfo {
 		c++
-		if !info.IsDir() {
-			files = append(files, path)
-		}
-		if c == 1 {
-			fmt.Printf("\nDirectory: %s\n", info.Name())
-		}
-		if c > 1 {
-			fmt.Printf("	FileName: %s\n", info.Name())
-		}
-		return nil
-	})
-	return files, err
+		file_path := root + "\\" + file.Name()
+		files = append(files, file_path)
+	}
+	return files, nil
 }

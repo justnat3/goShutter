e1111c7dc3cbf057af069a55b456b6bd18d7dbf8
justnat3 <nd2010@reedusa.com>
2020-08-31 23:23:14 -0500

    more features

--- main.go
+++ main.go
@@ -23,21 +23,25 @@ type hash struct {
 }
 
 func main() {
-	files, err := IOReadDir("C:\\Users\\Nathan Reed\\Downloads\\afreightdata\\afreightdata" + "\\")
+	files, dupespath, err := IOReadDir("C:\\Users\\Nathan Reed\\Downloads\\afreightdata\\afreightdata" + "\\")
 
 	if err != nil {
 		panic(err)
 	}
 
-	HashFiles(files)
+	HashFiles(files, dupespath)
 	fmt.Scanln("enter: ")
 }
 
 //HashFiles : take array of files and hash them
-func HashFiles(files []string) {
+func HashFiles(files []string, dupespath string) {
+
+	//TODO:
+	// Grab File name and append it to the end of the dupe_fileName
+	// hashes are the key in the hash table
+	// mapping members will not take place if a member exists in the hashtable
 
 	var readFile string
-	//var hashes []hash
 	const BlockSize = 64
 
 	m := make(map[string]string)
@@ -47,14 +51,16 @@ func HashFiles(files []string) {
 		readFile = files[i]
 		f, err := os.Open(readFile)
 		defer f.Close()
-
+		println(f.Name())
 		if err != nil {
 			log.Fatal("Could not open file")
 		}
 
 		buff := make([]byte, 512)
 		f.Read(buff)
-
+		newFile := dupespath + f.Name()
+		println(newFile)
+		println(dupespath)
 		switch {
 		case http.DetectContentType(buff) == "image/jpeg":
 			hasher := sha256.New()
@@ -66,9 +72,9 @@ func HashFiles(files []string) {
 
 			_, ok := m[hex.EncodeToString(sum)]
 			if ok == false {
-				println("False")
-			} else {
-				println("True")
+				continue
+			} else if ok == true {
+				CopyFile(readFile, newFile)
 			}
 
 			m[hex.EncodeToString(sum)] = readFile
@@ -84,9 +90,11 @@ func HashFiles(files []string) {
 
 			_, ok := m[hex.EncodeToString(sum)]
 			if ok == false {
-				println("False")
+				println("\nFalse\n")
 			} else {
-				println("True")
+				CopyFile(readFile, newFile)
+				fmt.Printf("SOURCE =>" + readFile + "\n")
+				fmt.Printf("\nDEST =>" + newFile + "\n")
 			}
 			m[hex.EncodeToString(sum)] = readFile
 			f.Close()
@@ -97,21 +105,66 @@ func HashFiles(files []string) {
 }
 
 //IOReadDir : Read in Directory and spit out file names + PATH
-func IOReadDir(root string) ([]string, error) {
-	fmt.Println("\n")
+func IOReadDir(root string) ([]string, string, error) {
 
 	var files []string
+	dupespath := root + "dupes\\"
+	if err, _ := os.Stat(dupespath); err == nil {
+		os.Mkdir(dupespath, os.FileMode(0522))
+	} else {
+		log.Println("Already Exists")
+	}
+
+	fmt.Println("\n")
 	fileInfo, err := ioutil.ReadDir(root)
 	c := 0
 
 	if err != nil {
-		return files, err
+		log.Fatal(err)
 	}
+
 	println("Scanning...  " + root + "/\n")
+
 	for _, file := range fileInfo {
 		c++
 		filePath := root + file.Name()
 		files = append(files, filePath)
 	}
-	return files, nil
+	return files, dupespath, nil
+}
+
+func CopyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %s", err)
+	}
+
+	out, err := os.Create(dst)
+	if err != nil {
+		in.Close()
+		return fmt.Errorf("Couldn't open dest file: %s", err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	in.Close()
+	if err != nil {
+		return fmt.Errorf("Writing to output file failed: %s", err)
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return fmt.Errorf("Sync error: %s", err)
+	}
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return fmt.Errorf("Stat error: %s", err)
+	}
+	err = os.Chmod(dst, si.Mode())
+	if err != nil {
+		return fmt.Errorf("Chmod error: %s", err)
+	}
+
+	return nil
 }

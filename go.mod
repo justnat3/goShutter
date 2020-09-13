65950175c4c0a7da2d66215a115fc9d7308b3f3f
justnat3 <nd2010@reedusa.com>
2020-09-13 16:35:12 -0500

    stabalized the cli interaction

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github/justnat3/goDupe
 
 go 1.15
+
+require github.com/thatisuday/commando v1.0.4
--- main.go
+++ main.go
@@ -11,36 +11,88 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/thatisuday/commando"
 )
 
 var (
+	dir   string
 	table = make(map[string]string)
 )
 
 func main() {
 
-	fileName, filePath, dupespath, progress, err := IOReadDir("C:\\Users\\Nathan Reed\\Desktop\\")
+	// wd : get working directory for help info section
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	WelcomeStr := "Current Working Directory: " + wd + "\n\nINFO:\n  - This program searches for dupes at the limitations of your drives write speed\n  - If you would like to use this program in your Current working directory. ' goDupe dir . ' is what you are looking for"
+
+	// Commando : Used to get args from user
+	commando.
+		SetExecutableName("GoDupe").
+		SetVersion("v0.5.2").
+		SetDescription(WelcomeStr)
+
+	commando.
+		Register("dir").
+		AddArgument("dir", "Path to desired directory", "").
+		SetShortDescription("path to Desired Directory").
+		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
+			dir = args["dir"].Value
+		})
+
+	commando.Parse(nil)
+
+	// if the user-input is "." we can assume that it is the current directory
+	// otherwise no dir was specified
+	if dir == "." {
+		res, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("\nError: %s", err)
+		}
+		dir = res
+		fmt.Printf("Current Working Directory: %s\n", dir)
+	}
+
+	// if user input was not '.' we can assume that what comes next is a directory.
+	// if this dir is not valid we can exit and alert the user
+	td, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		log.Fatal("Folder does not exist.")
+		log.Println(td)
+	}
+
+	// Read IOReadDir Comment
+	fileName, filePath, dupespath, progress, err := IOReadDir(dir)
+
+	//Get Items already in dupes folder. If Dupes does not exist we can create it.
 	itemsInDupes, err := IOReadDupeFolder(dupespath)
 	if err != nil {
 		panic(err)
 	}
 
+	// Read Hashing Comment
 	HashFiles(fileName, filePath, dupespath, progress)
 
+	//We can assume that the items-caught were not already in the dupes folder so -> substract the total
 	itemsCaught, err := IOReadDupeFolder(dupespath)
-
 	fmt.Printf("Items Caught: %s", strconv.Itoa(itemsCaught-itemsInDupes))
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// Do not escape if run from exe
 	fmt.Scanln("enter: ")
-
 }
 
 //HashFiles : take array of files and hash them
 func HashFiles(fileName []string, filePath []string, dupespath string, progress int) {
+	// 	1.	Iterate through the files that were read in -> during the ReadDir Phase
+	//	2.	Read in the first 512bytes of the file to grab magic bytes
+	//	3.	Read into the net/http library to detect the files MimeType
+	//	4.	If there is a match we check if there is a match in that location in the table
+	//	5. 	if the check returns that it is not in the table we add that key to a location
+	//	6.	if the check returns there was a match we put the file in the dupespath
 
 	progressTotal := progress
 	start := time.Now()
@@ -48,7 +100,7 @@ func HashFiles(fileName []string, filePath []string, dupespath string, progress 
 	for i := 0; i < len(filePath); i++ {
 		progress = progress - 1
 
-		fmt.Printf("Progress: [%s/%s]\n", strconv.Itoa(progress), strconv.Itoa(progressTotal))
+		fmt.Printf("Progress: [%s/%s]	\n", strconv.Itoa(progress), strconv.Itoa(progressTotal))
 
 		filePath := filePath[i]
 		fileName := fileName[i]
@@ -56,7 +108,7 @@ func HashFiles(fileName []string, filePath []string, dupespath string, progress 
 		f, err := os.Open(filePath)
 
 		if err != nil {
-			log.Fatal("Could not open file")
+			fmt.Println("Error in loop")
 		}
 		defer f.Close()
 
@@ -126,16 +178,17 @@ func IOReadDir(root string) ([]string, []string, string, int, error) {
 	var filePaths []string
 	var c int = 0
 
+	// if dupes path does not exist -> create it
 	dupespath := root + "dupes\\"
 	if err, _ := os.Stat(dupespath); err == nil {
 		os.Mkdir(dupespath, os.FileMode(0522))
 	} else {
-		log.Println("Already Exists")
+		log.Println("\n DupesDir: Already Exists")
 	}
 
 	fileInfo, err := ioutil.ReadDir(root)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Could not open file\n")
 	}
 
 	fmt.Println("Scanning...  " + root + "\\\n")
@@ -155,6 +208,7 @@ func IOReadDir(root string) ([]string, []string, string, int, error) {
 
 }
 
+//	IOReadDupeFolder : read in what is in the dupesfolder
 func IOReadDupeFolder(dupespath string) (int, error) {
 	var fileNames []string
 	var c int = 0
